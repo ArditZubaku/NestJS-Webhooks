41ad30fc80dd988775bf119e792b550e9431ca3d
ArditZubaku <zubaku92@gmail.com>
2023-10-18 13:09:51 +0200

    Example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/order')
+  createOrder(@Body() data) {
+    const order = this.appService.createOrder(data);
+
+    this.httpService
+      .post('https://webhook.site/3df9d29f-7bb4-4957-8cbf-5d1d5de5046f')
+      .subscribe({
+        complete() {
+          console.log('completed');
+        },
+        error(err) {
+          console.log(err);
+        },
+      });
+
+    return order;
   }
 }
